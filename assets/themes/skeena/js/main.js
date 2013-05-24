b45bf92093d8de7a5dcd7acedfec99bc490db095
Edwin Knuth <eknuth@ecotrust.org>
2013-05-24 12:42:39 -0700

    slide up popover toc container

--- assets/themes/skeena/js/main.js
+++ assets/themes/skeena/js/main.js
@@ -3,14 +3,42 @@
 'use strict';
     $(document).ready(function () {
 
-        var $title = $(".page-header").find('h2'),
+        var $title = $(".page-header").find('.title-name'),
+            $toc = $("#toc"),
             $window = $(window);
 
-        $(window).on('scroll', function () {
+        $toc.popover({
+            placement: "bottom",
+            trigger: "manual",
+            container: ".toc-section"
+        });
 
+        $toc.on('click', function (e) {
+            var delay = 1000, $popover, tocHeight;
+            
+            $toc.popover('toggle');
+            $popover = $('.toc-section').find('.popover');
+            $popover.find('.arrow').position({
+               of: $toc,
+               my: 'bottom center',
+               at: 'bottom'
+             });
+            tocHeight = $popover.closest('.toc-section').height()
+            $popover.height($popover.closest('.section').height() - tocHeight);
+            e.preventDefault();
+            
+            
+            $('html, body').stop().animate({
+                 scrollTop: $toc.offset().top - tocHeight + 25
+             }, delay,'easeInOutExpo');
+            
+            
+        });
+
+        $(window).on('scroll', function () {
             if ($window.scrollTop() > 64) {
                 $title.addClass('static-nav');
-            } else {
+            } else if ($window.scrollTop() < $title.offset().top) {
                 $title.removeClass('static-nav')
             }
         });    
--- index.md
+++ index.md
@@ -5,6 +5,21 @@ tagline: Supporting tagline
 class: cover
 ---
 {% include JB/setup %}
-<div class="section">
-  Some stuff
+<div class="toc-section">
+  <div class="masthead">
+    <div class="navbar">
+      <div>
+        <div class="container">
+          <ul class="nav">
+            <li><a href="javascript:void(0);" id="toc" data-placement="bottom" data-content="Test">01 Table of Contents</a></li>
+            <li><a href="#">02 Map</a></li>
+            <li></li>
+            <li></li>
+            <li><a href="#">03 About</a></li>
+            <li><a href="#">04 Issues</a></li>
+          </ul>
+        </div>
+      </div>
+    </div><!-- /.navbar -->
+  </div>
 </div>
