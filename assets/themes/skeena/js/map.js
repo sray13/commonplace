0efc3bc6064d6afbb029e727e85174414d66343d
Edwin Knuth <eknuth@ecotrust.org>
2013-07-07 22:30:28 -0700

    trying other leaflet label plugin

--- assets/themes/skeena/js/map.js
+++ assets/themes/skeena/js/map.js
@@ -1,13 +1,17 @@
 var center = {
-	lat: 54,
-	lon: -130
+    lat: 54,
+    lon: -130
 };
 var zoom = 7;
 
 // Create a map
 window.map = L.mapbox.map('map', null, { 
-	zoomControl: false,
-	keyboard: false
+    zoomControl: false,
+    keyboard: false,
+    maxBounds:[
+        [60.72, -117.07],
+        [44.59, -149.32]
+    ]
 });
 
 map.dragging.disable();
@@ -20,83 +24,92 @@ map.addLayer(L.tileLayer('http://tilestream.apps.ecotrust.org/v2/commonplace/{z}
 map.setView([center.lat, center.lon], zoom);
 
 function onEachFeature(feature, layer) {
-	var popupContent =  $("#" + feature.properties.popup).html();
-	layer.bindPopup(popupContent, {
+    var popupContent =  $("#" + feature.properties.popup).html();
+    layer.bindPopup(popupContent, {
         closeButton: true,
         minWidth: 320
     });
 }
 
 var voicesLayer = L.geoJson(voices, {
-	pointToLayer: function (feature, latlng) {
-		var image = feature.properties.image;
-		if (image === '') {
-			image = 'map_voice_wht_90.png';
-		}
-		return L.marker(latlng, {
-			icon: L.icon({
-				iconUrl: 'assets/themes/skeena/img/map/' + image,
-				iconSize: [32, 37],
-				iconAnchor: [16, 37],
-				popupAnchor: [0, -28]
-			})
-		});
-	},
-	onEachFeature: onEachFeature
+    pointToLayer: function (feature, latlng) {
+        var image = feature.properties.image;
+        if (image === '') {
+            image = 'map_voice_wht_90.png';
+        }
+        return L.marker(latlng, {
+            icon: L.icon({
+                iconUrl: 'assets/themes/skeena/img/map/' + image,
+                iconSize: [32, 37],
+                iconAnchor: [16, 37],
+                popupAnchor: [0, -28]
+            })
+        });
+    },
+    onEachFeature: onEachFeature
 });
 
 var imageLayer = L.geoJson(images, {
-	pointToLayer: function (feature, latlng) {
-		var image = feature.properties.image;
-		if (image === '') {
-			image = 'map_image_wht.png';
-		}
-		return L.marker(latlng, {
-			icon: L.icon({
-				iconUrl: 'assets/themes/skeena/img/map/' + image,
-				iconAnchor: [16, 37],
-				popupAnchor: [0, -28]
-			})
-		});
-	},
-	onEachFeature: onEachFeature
+    pointToLayer: function (feature, latlng) {
+        var image = feature.properties.image;
+        if (image === '') {
+            image = 'map_image_wht.png';
+        }
+        return L.marker(latlng, {
+            icon: L.icon({
+                iconUrl: 'assets/themes/skeena/img/map/' + image,
+                iconAnchor: [16, 37],
+                popupAnchor: [0, -28]
+            })
+        });
+    },
+    onEachFeature: onEachFeature
 });
 
 $.get('assets/themes/skeena/places/pipeline.geojson', function (data) {
-	layers.pipeline = new L.GeoJSON(JSON.parse(data), {
-		style: {
-			"color": "black",
-			"weight": 6,
-			"opacity": 1
-		}
-	});
+    layers.pipeline = new L.GeoJSON(JSON.parse(data), {
+        style: {
+            "color": "black",
+            "weight": 6,
+            "opacity": 1
+        }
+    });
 });
 
 $.get('assets/themes/skeena/places/majcities.geojson', function (data) {
-	layers.places = new L.GeoJSON(JSON.parse(data), {
-		pointToLayer: function (feature, latlng) {
-			return new L.marker(latlng).bindLabel(feature.properties.NAME, { noHide: true }).showLabel();
-		}
-	});
+    layers.places = new L.GeoJSON(JSON.parse(data), {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: 'assets/themes/skeena/img/map/map_image_wht.png',
+                    iconSize: [32, 37],
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -28],
+                    labelText: 'test'
+
+                })
+            });
+        }
+    });
 });
 
 
 var layers = {
-	voices: voicesLayer,
-	images: imageLayer
+    voices: voicesLayer,
+    images: imageLayer
 };
 
 
 $(document).ready(function () {
-	$('.leaflet-control-zoom').addClass('hidden');
-	$('.legend').on('click', 'li', function (e) {
-		var $target = $(e.target);
-		$target.toggleClass('layer-on');
+    $('.leaflet-control-zoom').addClass('hidden');
+    $('.legend').on('click', 'li', function (e) {
+        var $target = $(e.target);
+        $target.toggleClass('layer-on');
 
-		if (($target).hasClass('layer-on')) {
-			layers[$target.data('layer')].addTo(map);
-		} else {
-			map.removeLayer(layers[$target.data('layer')]);
-		}
-	});
+        if (($target).hasClass('layer-on')) {
+            layers[$target.data('layer')].addTo(map);
+        } else {
+            map.removeLayer(layers[$target.data('layer')]);
+        }
+    });
 });
